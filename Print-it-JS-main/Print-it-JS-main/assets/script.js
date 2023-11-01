6b1be59b865039e2e8b133956d460b7b92ce1c0e
Soranirasu <139244421+Soranirasu@users.noreply.github.com>
2023-11-01 14:55:21 +0100

    Début Projet

---
+++ Print-it-JS-main/Print-it-JS-main/assets/script.js
@@ -0,0 +1,45 @@
+const slides = [
+	{
+		"image":"slide1.jpg",
+		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		"image":"slide2.jpg",
+		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		"image":"slide3.jpg",
+		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		"image":"slide4.png",
+		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+]
+
+let position = 0;
+let slidesNumb = slides.length;
+
+
+let arrowR = document.querySelector('.arrow_right');
+let arrowL = document.querySelector('.arrow_left');
+
+arrowL.addEventListener('click', function(){
+	if (position == 0) {
+		position = slidesNumb - 1;
+	}
+	else {
+		position--;
+	}
+})
+
+arrowR.addEventListener('click', function(){
+	if (position == slidesNumb - 1) {
+		position = 0;
+	}
+	else {
+		position++;
+	}
+})
+
+console.log()
